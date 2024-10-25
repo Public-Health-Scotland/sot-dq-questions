64768f6285448bc8099a9defd5624b0bbc0b15b5
Angus-Morton <angus.morton@phs.scot>
2024-10-25 14:36:12 +0100

    Created template script. Defined styles. Added PHS logo

--- code/create_excel.R
+++ code/create_excel.R
@@ -14,17 +14,84 @@ library(readr)
 library(dplyr)
 library(lubridate)
 library(tidylog)
-library(rjson)
 library(openxlsx)
 
-#### Step 1 : import settings ----
+#### Step 1 : import out numbers ----
 
-settings <- fromJSON(file="settings.json")
+#### Step x : define styles ----
 
-#### Step x : pull out numbers ----
+s_title <- createStyle(
+  fontSize = 16,
+  textDecoration = "bold",
+)
 
-#### Step x : create excel spreadsheet ----
+s_subtitle <- createStyle(
+  fgFill = "#B8CCE4",
+  border = "bottom",
+  borderStyle = "thin"
+)
+
+s_table <- createStyle(
+  border = c("top", "bottom", "left", "right"),
+  borderStyle = "thin",
+  halign = "center"
+)
+
+s_table_header <- createStyle(
+  fgFill = "#B8CCE4",
+)
+
+s_priority <- createStyle(
+  textDecoration = "bold",
+)
+
+s_p_high <- createStyle(
+  fgFill = "#808080",
+  border = c("top", "bottom", "left", "right"),
+  borderStyle = "thin",
+)
+
+s_p_med <- createStyle(
+  fgFill = "#BFBFBF",
+  border = c("top", "bottom", "left", "right"),
+  borderStyle = "thin",
+)
+
+s_p_low <- createStyle(
+  fgFill = "#F2F2F2",
+  border = c("top", "bottom", "left", "right"),
+  borderStyle = "thin",
+)
+
+#### Step x : create xlsx file ----
+
+wb <- createWorkbook()
+
+modifyBaseFont(wb, fontSize = 12,
+               fontName = "Arial")
+
+addWorksheet(wb, "phs logo")
+showGridLines(wb, "phs logo", showGridLines = FALSE)
+
+insertImage(wb, "phs logo", "phs-logo.png",
+            startRow = 2, startCol = 8)
+
+addStyle(wb, "phs logo", s_title,
+         rows = 2, cols = 2)
+addStyle(wb, "phs logo", s_subtitle,
+         rows = 4, cols = 2)
+addStyle(wb, "phs logo", s_table_header,
+         rows = 6, cols = 2:6, gridExpand = TRUE)
+addStyle(wb, "phs logo", s_table,
+         rows = 7, cols = 2:6, gridExpand = TRUE)
+addStyle(wb, "phs logo", s_p_med,
+         rows = 8, cols = 2)
+
+#### Step x : write data to excel ----
+
+saveWorkbook(wb, "output/template.xlsx", overwrite = TRUE)
 
 
 
-#### Step x : write to excel ----
+
+
